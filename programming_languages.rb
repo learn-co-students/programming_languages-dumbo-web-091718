5042a57ff65782b7d98a7e8146387f8421d5360a
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-24 19:13:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
-  # your code here
+  hash_arr = []
+  style_arr = []
+  languages.each do |k, v|
+    style_arr << k 
+    v.keys.each do |el| 
+      ooh = languages[:oo].has_key?(el)
+      fun = languages[:functional].has_key?(el)
+
+      if ooh && fun 
+        style = style_arr
+      else
+        style = [k]
+      end
+
+      hash_arr << {el => {:type => v[el][:type], :style => style}}
+
+    end
+  end
+  return hash_arr.reduce Hash.new, :merge
 end
