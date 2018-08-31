d44048dd3664e0aaa151517a06d172f4d9edafc8
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 21:05:55 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
-def reformat_languages(languages)
-  # your code here
+languages.each do |top_key, language|
+#  puts "here is the top key:"
+#   puts top_key
+    language.each do |language, type|
+    puts "LANGUAGE:"
+    puts language
+    puts "TYPE"
+    puts type 
+   end 
+   
 end
