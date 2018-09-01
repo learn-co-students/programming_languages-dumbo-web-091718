7a52d11a5c18430cefe877a8f14e63a660681221
Nasser Sanou <nassersanou23@gmail.com>
2018-09-01 00:02:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,24 @@
+def reformat_languages(languages)
+ formatted = Hash.new()
+
 languages.each do |top_key, language|
-#  puts "here is the top key:"
-#   puts top_key
-    language.each do |language, type|
-    puts "LANGUAGE:"
-    puts language
-    puts "TYPE"
-    puts type 
-   end 
-   
-end
+
+    language.each do |langua, type|
+    # puts "LANGUAGE:"
+    # puts language
+    # puts "TYPE"
+    # puts type 
+     if !formatted.keys.include?(language)
+    #_________________
+     styles_array = Array.new().push(top_key)
+     #  p "FORMATED"
+     p formatted[langua] = { :type => type , :style => styles_array}
+     else
+        formatted[langua][:style].push(top_key)
+    end 
+   end
+  end
+  formatted
+end   
+
+print reformat_languages(languages)
