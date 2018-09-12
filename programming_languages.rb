74e0befb63ba566a96e3b894cf057909f1db6784
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-12 03:19:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  reformatted = {}
+  languages.each do |style, langs|
+    langs.each do |language, type|
+      reformatted[language] = type if !reformatted.has_key?(language)
+      if reformatted[language].has_key?(:style)
+        reformatted[language][:style] << style
+      else
+        reformatted[language][:style] = [style]
+      end
+    end
+  end
+  return reformatted
 end
