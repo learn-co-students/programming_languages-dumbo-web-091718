88524c46f96a1d1b928c50b734996c066dc018a6
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-17 04:12:21 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash  = {}
+  
+  languages.each do |oo_fun, lang|
+   lang.each do |lang_name, tipo | 
+    
+    
+    # if lang_name == oo_fun[lang]
+    new_hash[lang_name] = {style: [oo_fun]}         
+    new_hash[lang_name].merge!(tipo)
+      
+   
+   end 
+
+  end
+puts new_hash
 end
+
+reformat_languages(languages)
